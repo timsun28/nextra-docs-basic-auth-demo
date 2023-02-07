46081b6f2032fc6f80df28d0e7e5eb12b0078e5c
Timo Frionnet <tfrionnet@gmail.com>
2023-02-07 18:07:18 +0100

    Update to middleware

--- middleware.ts
+++ middleware.ts
@@ -10,11 +10,17 @@ import { NextResponse } from "next/server";
 export const middleware = withLocales((request) => {
     const basicAuth = request.headers.get("authorization");
     const url = request.nextUrl;
+    const contentRoute = /\.(jpe?g|svg|png|webmanifest|xml|ico|txt)$/.test(request.nextUrl.pathname);
+    if (contentRoute) return NextResponse.next();
     if (basicAuth) {
         const authValue = basicAuth.split(" ")[1];
         const [user, pwd] = atob(authValue).split(":");
         if (user === "4dmin" && pwd === "testpwd123") {
             return NextResponse.next();
+        } else {
+            request.headers.delete("authorization");
+            url.pathname = "/api/auth";
+            return NextResponse.rewrite(url);
         }
     }
     url.pathname = "/api/auth";
